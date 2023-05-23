02e1dc0740f301d69e40ed656250c75fcb2af6eb
17Akshar <aksharparmar2002@gmail.com>
2023-05-23 23:13:53 +0530

    MEAL PLANNING PAGE INITIALIZE

---
+++ frontend/app/MEAL PLANNING/mp.js
@@ -0,0 +1,16 @@
+function Routing(n){
+  if(n==1){
+    var list = document.getElementById('list')
+    list.classList.add('m-[0%]')
+  }
+  else if(n==2){
+    var meal = document.getElementById('meal')
+    meal.classList.add('-ml-[100%]')
+  }
+  else if(n==3){
+
+  }
+  else{
+
+  }
+}
